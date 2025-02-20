ba918a96a57b7c850f5f6e3605605fb1dd4dfd01
MarkCesium <danikhrenkov29@gmail.com>
2025-02-20 11:38:29 +0300

    Refactor code and create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps({ text: String })
+</script>
+
+<template>
+  <h1 class="lg:text-5xl md:text-4xl text-3xl text-fuchsia-500 py-5 mb-5 text-center font-mono">
+    {{ text }}
+  </h1>
+</template>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from '../components/Header.vue'
+</script>
+
+<template>
+  <main class="bg-zinc-900 h-screen">
+    <Header text="CesBin" />
+  </main>
+</template>
